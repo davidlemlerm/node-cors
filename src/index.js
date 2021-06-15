5303ba51e611f44a998e26b533fcb7f4b1c431d3
David Lemler <dlemler@pm.me>
2021-06-14 21:36:45 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+// Constants
+const port = 8080;
+// Express
+const express = require("express");
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+// CORS
+let allowedOrigins = ["http://localhost:8080"];
+const cors = require("cors");
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: [
+      "GET",
+      "HEAD",
+      "POST",
+      "PUT",
+      "DELETE",
+      "CONNECT",
+      "OPTIONS",
+      "TRACE",
+      "PATCH"
+    ],
+    exposedHeaders: ["Authorization", "*"],
+    credentials: true,
+    // 24 hours or 1 day
+    maxAge: 86400
+  })
+);
+// Helmet
+const helmet = require("helmet");
+app.use(
+  helmet.referrerPolicy({
+    policy: "strict-origin-when-cross-origin"
+  })
+);
+app.use(helmet.noSniff());
+/* Disabled so that CodeSandbox can
+iFrame responses from this server
+app.use(
+  helmet.frameguard({
+    action: "deny"
+  })
+);
+*/
+app.use(helmet.hidePoweredBy());
+app.use(helmet.xssFilter());
+
+// Routes
+app.get("/", (request, response) => {
+  response.status(200).json({
+    message: "CORS Is Working",
+    allowed: allowedOrigins,
+    hasOriginHeader: request.get("Origin") ? true : false
+  });
+});
+
+// Initialize App
+app.listen(port, () => {
+  console.log("Litening on port " + port);
+});
